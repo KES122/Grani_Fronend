61fadc0f154ea8042008d8588b2f6312929ee44b
Sergey <146075682+juss9@users.noreply.github.com>
2023-12-06 13:08:25 +0300

    commit2
    Добавление оставшихся компонентов main части страницы "Анкета"
    Редактирование оформления некоторых деталей из предыдущего комита

--- src/components/applicationOfPeople/Application.tsx
+++ src/components/applicationOfPeople/Application.tsx
@@ -1,12 +1,17 @@
 import ButtonsBellow from '../buttonsBelowProfile/ButtonsBelow'
+import JumpBottonStepOne from '../jumpButtonStep1/JumpButtonStep1'
+import TravelChapter from '../travelChapter/TravelChapter'
 import style from './application.module.scss'
 
 
 const Application = () => {
     return (
+
         <section className={style.sectionApplication}>
+            <JumpBottonStepOne/>
+            <TravelChapter />
             <div className={style.sectionApplication__HeaderSection}>
-                <h2 className={style.HeaderSection__titleAppli }>Оставьте заявку на участие в поездке</h2>
+                <h2 className={style.HeaderSection__titleAppli}>Оставьте заявку на участие в поездке</h2>
                 <div className={style.HeaderSection__Edit}>
                     <img className={style.Edit__editIco} src="public/editIco.svg" />
                     <a className={style.Edit__TextEdit} href='#'>Редактировать</a>
@@ -14,34 +19,34 @@ const Application = () => {
             </div>
             <div className={style.sectionApplication__applicationData}>
 
-               <div className={style.applicationData__photo}>
-                    {/* <input type="file" multiple accept="image/*" />*/ }
+                <div className={style.applicationData__photo}>
+                    {/* <input type="file" multiple accept="image/*" />*/}
                 </div>
 
-                    <div className={style.applicationData__user}>
-                        <p className={style.user__lab}>Фамилия Имя</p>
-                        <input className={style.user__input} placeholder="Иванов Иван" type="text"/>
-                    </div>
+                <div className={style.applicationData__user}>
+                    <p className={style.user__lab}>Фамилия Имя</p>
+                    <input className={style.user__input} placeholder="Иванов Иван" type="text" />
+                </div>
 
-                    <div className={style.applicationData__user}>
-                        <p className={style.user__lab}>Возраст</p>
-                        <input className={style.user__input} placeholder="777777" type="text"/>
-                    </div>
+                <div className={style.applicationData__user}>
+                    <p className={style.user__lab}>Возраст</p>
+                    <select className={style.user__input}/>
+                </div>
 
-                    <div className={style.applicationData__user}>
-                        <p className={style.user__lab}>Вес</p>
-                        <input className={style.user__input} placeholder="777777" type="text"/>
-                    </div>
+                <div className={style.applicationData__user}>
+                    <p className={style.user__lab}>Вес</p>
+                    <input className={style.user__input} placeholder="777777" type="text" />
+                </div>
 
-                    <div className={style.applicationData__user}>
-                        <p className={style.user__lab}>Аллергия</p>
-                        <input className={style.user__input} placeholder="Есть/Нет" type="text"/>
-                    </div>
+                <div className={style.applicationData__user}>
+                    <p className={style.user__lab}>Аллергия</p>
+                    <input className={style.user__input} placeholder="Есть/Нет" type="text" />
+                </div>
 
-                    <div className={style.applicationData__user}>
-                        <p className={style.user__lab}>Роль</p>
-                        <input className={style.user__input} placeholder="Список" type="text"/>
-                    </div>
+                <div className={style.applicationData__user}>
+                    <p className={style.user__lab}>Роль</p>
+                    <input className={style.user__input} placeholder="Список" type="text" />
+                </div>
             </div>
 
             <div className={style.sectionApplication__buttonSectionApplication}>
@@ -50,7 +55,7 @@ const Application = () => {
                 <button className={style.buttonSectionApplication__but} type='submit'>ОТПРАВИТЬ ЗАЯВКУ</button>
 
             </div>
-            <ButtonsBellow/>
+            <ButtonsBellow />
         </section>
 
     )
