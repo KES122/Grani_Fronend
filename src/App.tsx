b385d063f997a3b6fc7373d89aa4771fe359e0f4
Суковатов Никита Дмитриевич <117944244+Snich30@users.noreply.github.com>
2023-12-11 12:04:01 +0300

    navigating pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
 import './App.scss'
-import Home from './Pages/HomePage/Home'
-import Page2 from './Pages/Forum/Page2';
-import Page3 from './Pages/Forum2/Page3';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Home/>
-    <Router>
-      <Routes>
-        <Route path ="/" element={<Home/>}/>
-        <Route path ="/Forum" element={<Page2/>}/>
-        <Route path ="/Forum2" element={<Page3/>}/>
-      </Routes>
-    </Router>
-    </>
+    <Outlet/>
+    
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-// import './index.css'
-import "./App.scss"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./App.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/HomePage/Home";
+import Page2 from "./Pages/Forum/Page2.tsx";
 
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/Forum",
+        element: <Page2/>,
+      },
+    ],
+  },
+]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
