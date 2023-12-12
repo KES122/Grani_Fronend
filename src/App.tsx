3dfe36476d9726f3a8b66f2c7756a47d836b486f
Sergey <146075682+juss9@users.noreply.github.com>
2023-12-12 13:22:56 +0300

    commit12/12
    Небольшое изменение кнопки на странице шаг 1/1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
-import ApplicationPage3 from './components/applicationPage3/applicationPage3'
-//import Application from './components/applicationOfPeople/Application'
+// import ApplicationPage3 from './components/applicationPage3/applicationPage3'
+import Application from './components/applicationOfPeople/Application'
 
 
 function App() {
   return (
     //Коммент Application чтобы отобразить другую страницу для просмотра результата, пока только так(
     <>
-      {/*<Application />*/} 
-      <ApplicationPage3/>
+      <Application /> 
+      {/* <ApplicationPage3/> */}
     </>
   )
 }
